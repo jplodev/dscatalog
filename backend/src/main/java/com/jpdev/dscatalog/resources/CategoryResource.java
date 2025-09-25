4eef1c5c42b8d27eca5a749e7554d6ed7b6fb804
João Paulo Lima <jplopapa.dev@gmail.com>
2025-09-24 23:44:21 -0300

    Pagination

--- backend/src/main/java/com/jpdev/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/jpdev/dscatalog/resources/CategoryResource.java
@@ -3,6 +3,9 @@ package com.jpdev.dscatalog.resources;
 import com.jpdev.dscatalog.dto.CategoryDTO;
 import com.jpdev.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -18,8 +21,13 @@ public class CategoryResource {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll(){
-        List<CategoryDTO> list = service.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<CategoryDTO> list = service.findAllPaged(pageRequest);
         return ResponseEntity.ok(list);
     }
 
